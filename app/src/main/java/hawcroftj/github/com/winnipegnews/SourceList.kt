312983bb5f1b7d15f20a67f866214c2027b20518
Jadon H <hawcroftj@gmail.com>
2018-04-21 11:57:34 -0500

    Created sources.xml to manage news source URLs. Added a ListView to SourceList to display sources.

--- app/src/main/java/hawcroftj/github/com/winnipegnews/SourceList.kt
+++ app/src/main/java/hawcroftj/github/com/winnipegnews/SourceList.kt
@@ -2,8 +2,12 @@ package hawcroftj.github.com.winnipegnews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class SourceList : AppCompatActivity() {
+
+    private lateinit var lvSources: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,17 @@ class SourceList : AppCompatActivity() {
          * Free Press, Global News, CTV News, CBC Manitoba, Winnipeg Sun, Chris D
          */
 
+        // initialize views
+        lvSources = findViewById<ListView>(R.id.lvSources)
+    }
 
+    override fun onStart() {
+        super.onStart()
 
+        // pull news source URLs from sources.xml
+        var sourceUrlList = resources.getStringArray(R.array.news_sources)
+        // prepare simple adapter for lvSources
+        val sourceAdapter = ArrayAdapter(this, R.layout.source_row, sourceUrlList)
+        lvSources.adapter = sourceAdapter
     }
 }
