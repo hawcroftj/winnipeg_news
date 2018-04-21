87d3cdf7f203e39ebf8c63f26be4442554118f02
Jadon H <hawcroftj@gmail.com>
2018-04-21 11:59:47 -0500

    Created ItemList activity. This will display news items from 1 or more sources selected in SourceList.

---
+++ app/src/main/java/hawcroftj/github/com/winnipegnews/ItemList.kt
@@ -0,0 +1,18 @@
+package hawcroftj.github.com.winnipegnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+
+        /* TODO
+         * Format:
+         * Title, Post Date, Content, [optional] Image
+         */
+
+    }
+}
