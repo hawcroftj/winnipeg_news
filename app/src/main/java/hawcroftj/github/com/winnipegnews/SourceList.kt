32a6dd66269aea44adf208c749ffb570e6c958b3
Jadon H <hawcroftj@gmail.com>
2018-04-21 10:49:33 -0500

    Initial commit of a new Kotlin android application.

---
+++ app/src/main/java/hawcroftj/github/com/winnipegnews/SourceList.kt
@@ -0,0 +1,20 @@
+package hawcroftj.github.com.winnipegnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SourceList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_source_list)
+
+        /* TODO
+         * Potential Sources:
+         * Free Press, Global News, CTV News, CBC Manitoba, Winnipeg Sun, Chris D
+         */
+
+
+
+    }
+}
